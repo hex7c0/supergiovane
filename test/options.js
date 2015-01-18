dcc80048c2669fe8d44411b712719294b6e1f0c5
hex7c0 <carniellifrancesco@gmail.com>
2015-01-18 11:56:25 +0100

    fix test for nodejs

--- test/options.js
+++ test/options.js
@@ -14,14 +14,14 @@
  */
 // import
 try {
-    var supergiovane = require('../index.min.js'); // use
-    // require('supergiovane')
-    var request = require('supertest');
-    var assert = require('assert');
-    var fs = require('fs');
+  var supergiovane = require('..'); // use
+  // require('supergiovane')
+  var request = require('supertest');
+  var assert = require('assert');
+  var fs = require('fs');
 } catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
+  console.error(MODULE_NOT_FOUND);
+  process.exit(1);
 }
 
 /*
@@ -29,91 +29,92 @@ try {
  */
 describe('options', function() {
 
-    var app;
-    var r = __dirname + '/r.txt';
-    var s = __dirname + '/s.xml';
-    before(function(done) {
+  var app;
+  var r = __dirname + '/r.txt';
+  var s = __dirname + '/s.xml';
+  before(function(done) {
 
-        app = supergiovane({
-            port: 3001,
-            env: 'test',
-            dir: __dirname + '/',
-            referer: 'boh',
-            logger: false,
-            sitemap: false,
-            signature: {
-                token: 'Full'
-            },
-            debug: false
-        });
+    app = supergiovane({
+      port: 3001,
+      env: 'test',
+      dir: __dirname + '/',
+      referer: 'boh',
+      logger: false,
+      sitemap: false,
+      signature: {
+        token: 'Full'
+      },
+      debug: false
+    });
+    done();
+  });
+
+  describe('correct - should return 200 status code', function() {
+
+    it('custom index', function(done) {
+
+      request(app).get('/').expect(200, done);
+    });
+
+    it('package', function(done) {
+
+      request(app).get('/supergiovane/')
+          .expect('Server', /Nodejs\/[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2} \(/)
+          .set('Referer', 'boh').expect(200).end(function(err, res) {
+
+            if (err)
+              throw err;
+            assert.deepEqual(res.statusCode, 200);
+            var j = JSON.parse(res.text);
+            assert.deepEqual(j.name, 'supergiovane');
+            assert.deepEqual(j.versions['0.0.1'].main, 'index.min.js');
+            assert.deepEqual(j.license, 'GPLv3');
+            done();
+          });
+    });
+  });
+
+  describe('error - should return 301 status code', function() {
+
+    it('package different refer', function(done) {
+
+      request(app).get('/supergiovane/').set('Referer', 'mah')
+          .expect(301, done);
+    });
+  });
+
+  describe('exist files', function() {
+
+    it('logger', function(done) {
+
+      if (!fs.existsSync('route.log')) {
         done();
+      }
+      return;
     });
 
-    describe('correct - should return 200 status code', function() {
+    it('robots', function(done) {
 
-        it('custom index', function(done) {
-
-            request(app).get('/').expect(200, done);
-        });
-
-        it('package', function(done) {
-
-            request(app).get('/supergiovane/').expect('Server',
-                    /Nodejs\/0.[0-9]{1,2}.[0-9]{1,2} \(/).set('Referer', 'boh').expect(
-                    200).end(function(err, res) {
-
-                if (err)
-                    throw err;
-                assert.deepEqual(res.statusCode, 200);
-                var j = JSON.parse(res.text);
-                assert.deepEqual(j.name, 'supergiovane');
-                assert.deepEqual(j.versions['0.0.1'].main, 'index.min.js');
-                assert.deepEqual(j.license, 'GPLv3');
-                done();
-            });
-        });
+      if (!fs.existsSync(r)) {
+        done();
+      }
+      return;
     });
 
-    describe('error - should return 301 status code', function() {
+    it('sitemap', function(done) {
 
-        it('package different refer', function(done) {
-
-            request(app).get('/supergiovane/').set('Referer', 'mah').expect(301, done);
-        });
+      if (!fs.existsSync(s)) {
+        done();
+      }
+      return;
     });
 
-    describe('exist files', function() {
+    it('debug', function(done) {
 
-        it('logger', function(done) {
-
-            if (!fs.existsSync('route.log')) {
-                done();
-            }
-            return;
-        });
-
-        it('robots', function(done) {
-
-            if (!fs.existsSync(r)) {
-                done();
-            }
-            return;
-        });
-
-        it('sitemap', function(done) {
-
-            if (!fs.existsSync(s)) {
-                done();
-            }
-            return;
-        });
-
-        it('debug', function(done) {
-
-            if (!fs.existsSync('debug.log')) {
-                done();
-            }
-            return;
-        });
+      if (!fs.existsSync('debug.log')) {
+        done();
+      }
+      return;
     });
+  });
 });
